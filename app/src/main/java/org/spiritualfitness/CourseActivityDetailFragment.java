bdf865a3cab72a4b4499b4ba0cf2201ecd65b98a
Jesse Stebbins <jesse.w.stebbins@wellsfargo.com>
2015-04-21 20:52:45 -0500

    Initial checkin

---
+++ app/src/main/java/org/spiritualfitness/CourseActivityDetailFragment.java
@@ -0,0 +1,62 @@
+package org.spiritualfitness;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import org.spiritualfitness.dummy.DummyContent;
+
+/**
+ * A fragment representing a single CourseActivity detail screen.
+ * This fragment is either contained in a {@link CourseActivityListActivity}
+ * in two-pane mode (on tablets) or a {@link CourseActivityDetailActivity}
+ * on handsets.
+ */
+public class CourseActivityDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private DummyContent.DummyItem mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public CourseActivityDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_courseactivity_detail, container, false);
+
+        // Show the dummy content as text in a TextView.
+        if (mItem != null) {
+            ((TextView) rootView.findViewById(R.id.courseactivity_detail)).setText(mItem.content);
+        }
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/org/spiritualfitness/CourseActivityListActivity.java
@@ -0,0 +1,81 @@
+package org.spiritualfitness;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+
+/**
+ * An activity representing a list of CourseActivities. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched,
+ * lead to a {@link CourseActivityDetailActivity} representing
+ * item details. On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
+ * <p/>
+ * The activity makes heavy use of fragments. The list of items is a
+ * {@link CourseActivityListFragment} and the item details
+ * (if present) is a {@link CourseActivityDetailFragment}.
+ * <p/>
+ * This activity also implements the required
+ * {@link CourseActivityListFragment.Callbacks} interface
+ * to listen for item selections.
+ */
+public class CourseActivityListActivity extends FragmentActivity
+        implements CourseActivityListFragment.Callbacks {
+
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_courseactivity_list);
+
+        if (findViewById(R.id.courseactivity_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-large and
+            // res/values-sw600dp). If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
+
+            // In two-pane mode, list items should be given the
+            // 'activated' state when touched.
+            ((CourseActivityListFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.courseactivity_list))
+                    .setActivateOnItemClick(true);
+        }
+
+        // TODO: If exposing deep links into your app, handle intents here.
+    }
+
+    /**
+     * Callback method from {@link CourseActivityListFragment.Callbacks}
+     * indicating that the item with the given ID was selected.
+     */
+    @Override
+    public void onItemSelected(String id) {
+        if (mTwoPane) {
+            // In two-pane mode, show the detail view in this activity by
+            // adding or replacing the detail fragment using a
+            // fragment transaction.
+            Bundle arguments = new Bundle();
+            arguments.putString(CourseActivityDetailFragment.ARG_ITEM_ID, id);
+            CourseActivityDetailFragment fragment = new CourseActivityDetailFragment();
+            fragment.setArguments(arguments);
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.courseactivity_detail_container, fragment)
+                    .commit();
+
+        } else {
+            // In single-pane mode, simply start the detail activity
+            // for the selected item ID.
+            Intent detailIntent = new Intent(this, CourseActivityDetailActivity.class);
+            detailIntent.putExtra(CourseActivityDetailFragment.ARG_ITEM_ID, id);
+            startActivity(detailIntent);
+        }
+    }
+}
